6f311addf4428c433dcb67dcea6402d66183aec1
DLamarG <demondg8@gmail.com>
2024-09-24 16:25:54 -0500

    configured database for postgres

--- career_tracker/career_tracker/settings.py
+++ career_tracker/career_tracker/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'career_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "career_tracker_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  
+        "PORT": 5432, 
     }
 }
 
